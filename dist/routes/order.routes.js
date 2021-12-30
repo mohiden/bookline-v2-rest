2ea0f10b95c1f7af66b552ce787afe74ec5c65af
mohidenabdulkarim <mohidendj@gmail.com>
2021-12-30 03:59:08 +0300

    wip

--- dist/routes/order.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const middleware_1 = require("../middleware");
-const resources_1 = require("../resources");
-const router = (0, express_1.Router)();
-router.post("/", (0, middleware_1.validateResource)(resources_1.createOrderSchema), resources_1.createOrderHandler);
-exports.default = router;
-//# sourceMappingURL=order.routes.js.map
