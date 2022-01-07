d84f4d8d8b53dda50f2f5789152d645c32b45819
mohidenabdulkarim <mohidendj@gmail.com>
2022-01-07 23:02:05 +0300

    wip

--- dist/index.js
+++ dist/index.js
@@ -12,7 +12,7 @@ const cors_1 = __importDefault(require("cors"));
 async function main() {
     const app = (0, express_1.default)();
     app.use((0, cors_1.default)({
-        origin: ['http://localhost:3000', 'https://83f4cb15.booklinev2-web.pages.dev'],
+        origin: ['http://localhost:3000', 'https://booklinev2-web.pages.dev'],
     }));
     app.use(express_1.default.json());
     app.use(middleware_1.deserializeUser);
